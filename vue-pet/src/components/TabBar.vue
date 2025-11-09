dd7862e09ff7d595afae9423398e493e08d6ca69
rongyan4 <2603726252@qq.com>
2025-11-09 15:49:28 +0800

    tabbar布局

--- vue-pet/src/components/TabBar.vue
+++ vue-pet/src/components/TabBar.vue
@@ -3,7 +3,8 @@
     <ul>
       <li v-for="(item, index) in tabbarList"
         :key="index"
-        @click= "switchItem(item.path)">
+        @click= "switchItem(item.path)"
+        >
         <img :src="item.icon">
         <span>{{item.title}}</span>
       </li>
@@ -16,10 +17,10 @@ import { ref } from "vue";
 import { useRoute, useRouter } from 'vue-router';
 const router = useRouter()
 const tabbarList = ref([
-  {title: "首页",path: "/",icon: "./images/home.png",selectIcon: "./images/home-select.png",},
-  {title: "分类",path: "/list",icon: "./images/list.png",selectIcon: "./images/list-select.png",},
-  {title: "购物车",path: "/car",icon: "./images/car.png",selectIcon: "./images/car-select.png",},
-  {title: "我的",path: "/my",icon: "./images/my.png",selectIcon: "./images/my-select.png",},
+  {title: "首页",path: "/",icon: "../images/tabbar/home.png",selectIcon: "./images/home-select.png",},
+  {title: "分类",path: "/list",icon: "../images/tabbar/list.png",selectIcon: "./images/list-select.png",},
+  {title: "购物车",path: "/car",icon: "../images/tabbar/cart.png",selectIcon: "./images/cart-select.png",},
+  {title: "我的",path: "/my",icon: "../images/tabbar/my.png",selectIcon: "./images/my-select.png",},
 ])
 const switchItem = (path) => {
   router.replace(path)
@@ -32,7 +33,29 @@ const switchItem = (path) => {
     left: 0;
     bottom: 0;
     width: 100%;
-    height: 50px;
-    
+    height: 2.2rem;
+    background-color: plum;
+    z-index: 999;
+    ul{
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      width: 100%;
+      height: 100%;
+      li{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content:center;
+        img{
+          height: 1rem;
+          width: 1rem;
+        }
+        span{
+          font-size: 0.45rem;
+          color: #999;
+        }
+      }
+    }
   }
 </style>
