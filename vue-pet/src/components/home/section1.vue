ac5db038df3f19267dbadc72e6595413107f7a86
rongyan4 <2603726252@qq.com>
2025-11-13 10:57:52 +0800

    swiper

---
+++ vue-pet/src/components/home/section1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="section1">
+    <swipe :autoplay="3000" lazy-render>
+      <swipe-item v-for="image in images" :key="image">
+        <img style="width:100%; height:100%" :src="image" />
+      </swipe-item>
+    </swipe>
+
+  </div>
+</template>
+
+<script setup>
+  import {Swipe, SwipeItem} from 'vant';
+  import {ref, reactive} from 'vue';
+  
+ const images = [
+  "/images/banner/banner1.jpg",
+  "/images/banner/banner2.jpg",
+  
+];
+
+</script>
+
+<style lang="scss">
+.swiper{
+  height: 400rpx;
+}
+</style>
--- vue-pet/src/views/HomeView.vue
+++ vue-pet/src/views/HomeView.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="home">
     <Header></Header>
+    <div class="content">
+      <tabs v-model:active="active">
+        <tab 
+        v-for="(item, index) in tabList" 
+        :title="item.title"
+        :key="item.type">
+          <div v-if="active === 0">
+            <section1></section1>
+          </div>
+          <div v-if="active === 1">
+            内容2
+          </div>
+          <div v-if="active === 2">
+            内容3
+          </div>
+        </tab>
+      </tabs>
+    <Tabbar></Tabbar>
+    </div>
   </div>
-  <div class="content">
-    <tabs v-model:active="active">
-      <tab v-for="index in 8" :title="'标签 ' + index">
-        内容 {{ index }}
-      </tab>
-    </tabs>
-  </div>
-  <Tabbar></Tabbar>
 </template>
 
 <script setup>
 import Tabbar from "@/components/TabBar.vue";
 import Header from "@/components/home/Header.vue"
+import section1 from "@/components/home/section1.vue"
 import { ref } from 'vue';
 import { Tab,Tabs } from 'vant';
 const active = ref(0);
+const tabList = ref([
+  { type:1 , title:"推荐"},
+  { type:2 , title:"推荐"},
+  { type:3 , title:"推荐"},
+])
 
 </script>
 
